ebb921ac00a5e0ae5403d2d6d8601129c2b4452c
SheerazIsmail <sheerazi539@gmail.com>
2025-03-26 20:09:34 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+import turtle
+
+# creating canvas 
+turtle.Screen().bgcolor("purple")
+
+sc = turtle.Screen()
+sc.setup(700, 800)
+turtle.title("Welcome to the  Turtle Window")
+
+# turtle object creation
+board = turtle.Turtle()
+
+# creating a square
+for i in range(4):
+    board.forward(200)
+    board.left(90)
+    i = i+1
+
+
+
+
+  
